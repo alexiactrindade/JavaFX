c69c3ff7a1de15227c9bda517932a93bc32fde09
alexiactrindade <alexiadacostatrindade@gmail.com>
2026-05-23 08:45:27 -0300

    first commit

---
+++ src/main/java/com/senai/MainApp.java
@@ -0,0 +1,21 @@
+package com.senai;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
---
+++ src/main/java/com/senai/controller/MainController.java
@@ -0,0 +1,4 @@
+package com.senai.controller;
+
+public class MainController {
+}
